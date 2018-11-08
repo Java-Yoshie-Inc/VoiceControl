748ca6b2b5eb4bbc308968c64377d3bfedaaf6e2
Java-Yoshie-Inc <44732127+Java-Yoshie-Inc@users.noreply.github.com>
2018-11-08 16:44:01 +0100

    df

--- src/grammar/ActivationWord.java
+++ src/grammar/ActivationWord.java
@@ -5,6 +5,7 @@ public enum ActivationWord {
 	Computer(new Synonyms(new String[] { "concerts", "compute", "computers", "conduit", "come to a jerk", "and", "i'm sure", "can cure", "kosher", "compare", "club", "computer", "come shop", "crunches", "come to", "come true", "i'm cured", "clump you okay", "clump you would have" })), 
 	John(new Synonyms(new String[] { "john", "johnny", "jonny", "the shop", "jill", "sure", "the op", "the job", "job", "jody", "dont't", "joan"})), 
 	Hey(new Synonyms(new String[] {"hey", "a", "eight", "and", "heh"})), 
+	YOSHIE(new Synonyms(new String[] {"no shit", "trashy", "oh shit", "josh", "jesse", "duffy", "judging", "java", "job", "yoshi", "yeah", "yeah she", "does she", "tough shit", "yeah shit", "catchy", "gosh"})), 
 	;
 	
 	private Synonyms synonyms;
