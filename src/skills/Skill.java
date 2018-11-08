2c38f369daecb70d30054c60b58fb108a771f86f
Java-Yoshie-Inc <44732127+Java-Yoshie-Inc@users.noreply.github.com>
2018-11-08 16:32:20 +0100

    df

---
+++ src/skills/Skill.java
@@ -0,0 +1,24 @@
+package skills;
+
+abstract class Skill extends Thread {
+	
+	Skill() {
+		
+	}
+	
+	@Override
+	public void run() {
+		super.run();
+	}
+	
+	protected String waitForInput() {
+		while(true) {
+			
+		}
+	}
+	
+	public void sendInput(String input) {
+		
+	}
+	
+}
---
+++ src/skills/Skills.java
@@ -0,0 +1,13 @@
+package skills;
+
+import java.util.ArrayList;
+
+public class Skills extends ArrayList<Skill> {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public Skills() {
+		
+	}
+	
+}
