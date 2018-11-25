f4bbdfaba61ddfacbefabd7473f0636d58e04d0f
JavaYoshie Inc <40612283+Yoshie2000@users.noreply.github.com>
2018-11-25 16:22:18 +0100

    asd

--- src/main/SpeechRecognizer.java
+++ src/main/SpeechRecognizer.java
@@ -24,8 +24,8 @@ public class SpeechRecognizer {
 	private String speechRecognitionResult; //This String contains the Result that is coming back from SpeechRecognizer
 	
 	private Computer generator;
-//	private Synonyms computerSynonyms = new Synonyms(new String[] {"concerts", "compute", "computers", "conduit", "come to a jerk", "and", "i'm sure", "can cure", "kosher", "compare", "club", "computer", "come shop", "crunches", "come to", "come true", "i'm cured", "clump you okay", "clump you would have"});
-	private Synonyms nameSynonyms = new Synonyms(new String[] {"no shit", "trashy", "oh shit", "josh", "jesse", "duffy", "judging", "java", "job", "yoshi", "yeah", "yeah she", "does she", "tough shit", "yeah shit", "catchy", "gosh"} );
+	private Synonyms nameSynonyms = new Synonyms(new String[] {"concerts", "compute", "computers", "conduit", "come to a jerk", "and", "i'm sure", "can cure", "kosher", "compare", "club", "computer", "come shop", "crunches", "come to", "come true", "i'm cured", "clump you okay", "clump you would have"});
+	//private Synonyms nameSynonyms = new Synonyms(new String[] {"no shit", "trashy", "oh shit", "josh", "jesse", "duffy", "judging", "java", "job", "yoshi", "yeah", "yeah she", "does she", "tough shit", "yeah shit", "catchy", "gosh"} );
 	
 	private boolean isEnabled = false, hasNextQuestion = false;
 	
@@ -219,7 +219,7 @@ public class SpeechRecognizer {
 			Voice.say("Yes?", false, true);
 			isEnabled = true;
 		} else {
-			System.out.println("Cant understand: " + speech);
+			System.out.println("Can't understand: " + speech);
 		}
 	}
 	
